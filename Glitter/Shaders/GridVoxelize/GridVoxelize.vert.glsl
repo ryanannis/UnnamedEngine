26939a288447c96ed7b19bcf342238ab5c6f00bf
minimumcut <tinnymint@gmail.com>
2017-10-22 14:00:26 -0700

    Add Crytek 'Sponza' level for testing

--- Glitter/Shaders/GridVoxelize/GridVoxelize.vert.glsl
+++
@@ -1,8 +0,0 @@
-#version 450 core
-layout (location = 0) in vec4 a_vert;
-
-void main()
-{
-	// Passthrough VS
-	color = vec4(1.f, 0.f, 0.f, 0.f);z
-}
--- Glitter/Sources/Sources (2)/Sources/Engine/Base/Components/RenderComponent.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Engine/Base/Types/Component.h"
-
-class RenderComponent : Component {
-
-};
