7d4c665aff3750a072ca2cf3e36b862f7668e3c8
minimumcut <tinnymint@gmail.com>
2017-10-22 14:56:07 -0700

    Add common header - just has branch prediction hint macros for now

---
+++ Glitter/Sources/Engine/Base/Common/Common.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// Branch prediction hints
+#define LIKELY(condition) __builtin_expect(static_cast<bool>(condition), 1)
+#define UNLIKELY(condition) __builtin_expect(static_cast<bool>(condition), 0)
