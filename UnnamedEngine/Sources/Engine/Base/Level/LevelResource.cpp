68e8839a16c703c1c24306ca109137f69fbe2a21
minimumcut <tinnymint@gmail.com>
2017-11-12 17:45:51 -0800

    Skeleton for LevelResource

---
+++ UnnamedEngine/Sources/Engine/Base/Level/LevelResource.cpp
@@ -0,0 +1,37 @@
+#include "LevelResource.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "Engine/Base/Resource/Serializer.h"
+#include "Engine/Base/Resource/ResourceManager.h"
+#include "Engine/Base/Resource/PropParser.h"
+#include "Engine/Base/Managers/EntityAdmin.h"
+
+LevelResource::LevelResource(std::string uri) :
+	Resource(uri),
+	mReady(false)
+{};
+
+bool LevelResource::IsReady() const
+{
+	return(mReady);
+}
+
+void LevelResource::Load(Ptr<ResourceManager> resourceManager)
+{
+	const URI uri = URI(GetURI());
+
+	/* todo: stick this somewhere else (probably res manager)
+	* as multiple types of resource are maybe dependent on a
+	* single file in the future */
+	std::string fullURL = uri.GetFilePath();
+	std::ifstream f(fullURL);
+	std::stringstream buffer;
+	buffer << f.rdbuf();
+
+	std::optional<PropTree> propTree = PropParser::Parse(buffer.str());
+
+	mReady = true;
+}
---
+++ UnnamedEngine/Sources/Engine/Base/Level/LevelResource.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Engine/Base/Common/Common.h"
+#include "Engine/Base/Types/Entity.h"
+#include "Engine/Base/Resource/Resource.h"
+#include "Engine/Base/Resource/PropTree.h"
+#include "Engine/Base/Types/StaticComponent.h"
+
+class EntityAdmin;
+
+class LevelResource : public Resource
+{
+public:
+	LevelResource(std::string uri);
+	virtual bool IsReady() const override;
+	virtual void Load(Ptr<ResourceManager> manager) override;
+
+private:
+	bool mReady;
+};
